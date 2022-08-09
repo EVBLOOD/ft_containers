683289d25e388e7562fa54d19391da47c56950e5
Saad Akllam <sakllam@e3r7p11.1337.ma>
2022-08-09 20:05:48 +0100

    something new! I don't know what

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -6,17 +6,15 @@
 /*   By: sakllam <sakllam@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 14:39:16 by sakllam           #+#    #+#             */
-/*   Updated: 2022/08/09 18:35:09 by sakllam          ###   ########.fr       */
+/*   Updated: 2022/08/09 20:03:11 by sakllam          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #pragma once
-
+#include <memory>
 namespace ft
 {
-    #include <memory>
-
     template<class T, class alloc = std::allocator<T> >
     class vector
     {
@@ -24,16 +22,18 @@ namespace ft
         typedef     alloc       allocator_type;
         typedef     T           value_type;
         typedef     size_t       size_type;
-        type
-        int _size;
-        T   *all;
+        // well we found something!
+            static_assert(std::is_same<typename allocator_type::value_type, value_type>::value, "Error in types: the allocater and the value");
+        // attributes
+        // int _size;
+        // T   *all;
         // typedef ft_vector<T, alloc> * iterator;
         public:
                                 // constracting and deconstracting
 
-            explicit vector (const allocator_type& alloc = allocator_type());  // default constra
+            explicit vector (const allocator_type& _alloc = allocator_type());  // default constra
             explicit vector (size_type n, const value_type& val = value_type(),
-                 const allocator_type& alloc = allocator_type()); // fill constra
+                 const allocator_type& _alloc = allocator_type()); // fill constra (comlicated types)
             vector (const vector& x); // copy constra
             // left one constract (range)
             ~vector(); // deconstra
