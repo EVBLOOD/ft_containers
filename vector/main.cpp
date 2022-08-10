9e4499429072bdb26caecf08ddb93e79280e972b
Saad Akllam <sakllam@e3r7p9.1337.ma>
2022-08-10 18:58:48 +0100

    push_back | operator[] | capacity | access elements | fixing other things

--- vector/main.cpp
+++ vector/main.cpp
@@ -6,11 +6,11 @@
 /*   By: sakllam <sakllam@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/05 17:34:57 by sakllam           #+#    #+#             */
-/*   Updated: 2022/08/10 16:32:41 by sakllam          ###   ########.fr       */
+/*   Updated: 2022/08/10 17:15:14 by sakllam          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include <vector>
+#include <vector>
 #include "vector.hpp"
 #include <iostream>
 
@@ -22,15 +22,15 @@ int main()
     // cout << x.max_size() << "\n"; 
     // x.push_back(1);
     // cout << x.max_size() << "\n";
-    ft::vector<int> x(1, 99);
+    // ft::vector<int> x(1, 99);
     // x.push_back(10);
     // x.push_back(10);
     // x.push_back(10);
     // x.push_back(10);
     // x.push_back(10);
     // x.reserve(5);
-    std::cout << x[0] << "\n";
-    std::cout << x[25] << "\n";
+    // std::cout << x[0] << "\n";
+    // std::cout << x[25] << "\n";
     // std::cout << x[5] << "\n";
     // std::cout << x[7] << "\n";
     // std::cout << x[8] << "\n";
@@ -40,4 +40,30 @@ int main()
     // std::cout << x.capacity() << "\n";
     // ft::vector<int> *xb = new ft::vector<int>;
     // std::cout << (*xb)[100] << "\n";
+    /// empty testing
+    // std::cout <<  
+    std::vector<int> x(-1);
+    x.push_back(0);
+    x.push_back(1);
+    x.push_back(2);
+    x.push_back(3);
+    // x.push_back(4);
+    std::cout << x[0] << "\n";
+    std::cout << x[1] << "\n";
+    std::cout << x[2] << "\n";
+    std::cout << x[3] << "\n";
+    std::cout << x[4] << "\n";
+    std::cout << x.capacity() << "\n";
+    std::cout << x.empty() << "\n";
+    x.reserve(-1);
+    std::cout << x.capacity() << "\n";
+    std::cout << x.empty() << "\n";
+    std::cout << x[0] << "\n";
+    std::cout << x[1] << "\n";
+    std::cout << x[2] << "\n";
+    std::cout << x[3] << "\n";
+    std::cout << x[4] << "\n";
+    std::cout << x[5] << "\n";
+    std::cout << x[6] << "\n";
+    std::cout << x[7] << "\n";
 }
--- vector/vector.hpp
+++ vector/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: sakllam <sakllam@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 14:39:16 by sakllam           #+#    #+#             */
-/*   Updated: 2022/08/10 16:31:12 by sakllam          ###   ########.fr       */
+/*   Updated: 2022/08/10 18:55:23 by sakllam          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -118,6 +118,8 @@ namespace ft
             explicit vector (const allocator_type& _alloc = allocator_type())  // default constra
             {
                 my_vec = NULL;
+                _currSize = 0;
+                _capacity = 0;
             }
             explicit vector (size_type n, const value_type& val = value_type(),
                  const allocator_type& ac = allocator_type()) : _alloc(ac) // fill constra (comlicated types)
@@ -135,9 +137,9 @@ namespace ft
                         i++;
                     }
                 }
-                catch (std::exception e)
+                catch (std::length_error e)
                 {
-                    std::cerr << "my_vector: ERROR in fill const \n"; 
+                    throw std::length_error(": vector");
                 }
             }
             vector (const vector& x) // copy constra
@@ -155,21 +157,42 @@ namespace ft
                         i++;
                     }
                 }
-                catch (std::exception e)
+                catch (std::length_error e)
                 {
-                    std::cerr << "my_vector: ERROR in copy const \n"; 
+                    throw std::length_error(": vector");
                 }
             }
             ~vector() // deconstra
             {
-                // allocator_type::d(my_vec);
+                if (my_vec)
+                {
+                    int i = 0;
+                    while (i < _currSize)
+                    {
+                        _alloc.destroy(&(my_vec[i]));
+                        i++;
+                    }
+                    _alloc.deallocate(my_vec, _capacity);
+                }
             }
             // left one constract (range)
                                 // operators
             vector& operator= (const vector& x) //operator=
             {
+                if (my_vec)
+                {
+                    int i = 0;
+                    while (i < _currSize)
+                    {
+                        _alloc.destroy(&(my_vec[i]));
+                        i++;
+                    }
+                    _alloc.deallocate(my_vec, _capacity);
+                }
                 this->_currSize = x._currSize;
                 this->_capacity = x._capacity;
+                my_vec = _alloc.allocate(sizeof(value_type) * _capacity);
+                memccpy(my_vec, x.my_vec, sizeof(value_type) * _currSize);
                 return this;
             }
             reference operator[] (size_type n) //operator[]
@@ -181,24 +204,90 @@ namespace ft
                 return my_vec[n];
             }
                                 // capacity
-            size_type size() const;
-            size_type max_size() const;
+            size_type size() const // vector current size
+            {
+                return (_currSize);
+            }
+            size_type capacity() const // vector current capacity
+            {
+                return (_capacity);
+            }
+            bool empty() const // is vector empty?
+            {
+                return (_currSize);
+            }
+            void reserve (size_type n) // TODO:
+            {
+                if (n <= _capacity)
+                    return ;
+                value_type   *new_vec;
+                new_vec = _alloc.allocate(sizeof(value_type) * n, 0);
+                memset(new_vec, 0, sizeof(value_type) * n);
+                memccpy(new_vec, my_vec, sizeof(value_type) * _currSize);
+                _alloc.deallocate(my_vec);
+                my_vec = new_vec;
+                _capacity = n;
+            }
             void resize (size_type n, value_type val = value_type());
-            size_type capacity() const;
-            bool empty() const;
-            void reserve (size_type n);
                                 // access elements
-            reference at (size_type n);
-            const_reference at (size_type n) const;
-            reference front();
-            const_reference front() const;
-            reference back();
-            const_reference back() const;
+            reference at (size_type n)
+            {
+                if (n > _currSize)
+                    throw std::out_of_range("vector: ");
+                return (my_vec[n]);
+            }
+            const_reference at (size_type n) const
+            {
+                if (n > _currSize)
+                    throw std::out_of_range("vector: ");
+                return (my_vec[n]);
+            }
+            reference front()
+            {
+                return (my_vec[0]);
+            }
+            const_reference front() const
+            {
+                return (my_vec[0]);
+            }
+            reference back()
+            {
+                return (my_vec[_currSize - 1]);
+            }
+            const_reference back() const
+            {
+                return (my_vec[_currSize - 1]);
+            }
                                 //  Modifiers
             // template <class InputIterator>
             // void assign (InputIterator first, InputIterator last);	
             // void assign (size_type n, const value_type& val);
-            void push_back (const value_type& val);
+            void push_back (const value_type& val)
+            {
+                if (_currSize == _capacity)
+                {
+                    if (_capacity == 0 || _capacity == 1)
+                        _capacity++;
+                    else
+                        _capacity = _capacity * 2;
+                    value_type *nv = _alloc.allocate(sizeof(value_type) * _capacity);
+                    memset(nv, 0, sizeof(value_type) * _capacity);
+                    if (my_vec)
+                    {
+                        memccpy(nv, my_vec, sizeof(value_type) * _currSize);
+                        int i = 0;
+                        while (i < _currSize)
+                        {
+                            _alloc.destroy(&(my_vec[i]));
+                            i++;
+                        }
+                        _alloc.deallocate(my_vec);
+                    }
+                    my_vec = nv;
+                }
+                my_vec[_currSize] = val;
+                _currSize++;
+            }
             void pop_back();
             // iterator insert (iterator position, const value_type& val);
             // void insert (iterator position, size_type n, const value_type& val);
