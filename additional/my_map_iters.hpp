7c06ffe6a5f4abff35845255f5577cd07083fc38
Saad Akllam <sakllam@e2r6p15.1337.ma>
2022-09-20 18:59:37 +0100

    start => reverse iters

--- additional/my_map_iters.hpp
+++ additional/my_map_iters.hpp
@@ -6,12 +6,13 @@
 /*   By: sakllam <sakllam@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 14:29:37 by sakllam           #+#    #+#             */
-/*   Updated: 2022/09/20 18:39:15 by sakllam          ###   ########.fr       */
+/*   Updated: 2022/09/20 18:55:47 by sakllam          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include "iterators_traits.hpp"
+#include "Red-Black-Tree/Tree_implemeted.hpp"
 
 namespace ft
 {
@@ -47,7 +48,7 @@ namespace ft
                 itr &operator=(const itr &x)
             {
                     if (&x != this)
-                        data = x.data;
+                        data->value = x.data->value;
                     return *this;
             }
             reference operator*() const
@@ -62,40 +63,121 @@ namespace ft
             
             My_Iter_map &operator++()
             {
-                data->next();
+                data = RedBlackTree<T>::next(data);
                 return *this;
             }
             
             My_Iter_map operator++(int)
             {
                 My_Iter_map temp(data);
-                ++data;
+                data = RedBlackTree<T>::next(data);
                 return temp;
             }
 
             My_Iter_map &operator--()
             {
-                --data;
+                data = RedBlackTree<T>::prev(data);
                 return *this;
             }
             
             My_Iter_map operator--(int)
             {
                 My_Iter_map temp(data);
-                --data;
+                data = RedBlackTree<T>::prev(data);
                 return temp;
             }
 
             bool operator==(const pointer cmp) const
             {
-                return data == cmp;
+                return data->value->first == cmp->value->first;
             }
 
             bool operator!=(const pointer cmp) const
             {
-                return data != cmp;
+                return data->value->first != cmp->value->first;
             }
             
     };
+    template<class T>
+    class MyRev_Iter_map
+    {
+        private:
+            MyRev_Iter_map(const long x) {
+                (void)x;
+            };
+            T *data;
 
+        public:
+            typedef typename ft::iterator_traits<T *>::value_type           value_type;
+            typedef typename ft::iterator_traits<T *>::difference_type      difference_type;
+            typedef typename ft::iterator_traits<T *>::pointer              pointer;
+            typedef typename ft::iterator_traits<T *>::reference            reference;
+            typedef typename ft::iterator_traits<T *>::iterator_category    iterator_category;
+
+            template<class iter>
+                MyRev_Iter_map(const iter &x) : data(&(*x)) {};
+
+            MyRev_Iter_map(T *ptr) : data(ptr) {};
+
+            ~MyRev_Iter_map() {};
+            
+            MyRev_Iter_map() : data(NULL) {};
+            
+            bool operator==(const MyRev_Iter_map &in) const { return in.data == data; };
+            bool operator!=(const MyRev_Iter_map &in) const { return !(in.data == data); };
+
+            template<class itr>
+                itr &operator=(const itr &x)
+            {
+                    if (&x != this)
+                        data->value = x.data->value;
+                    return *this;
+            }
+            reference operator*() const
+            {
+                return *data;
+            }
+            
+            reference operator->() const
+            {
+                return *data;
+            }
+            
+            MyRev_Iter_map &operator++()
+            {
+                data = RedBlackTree<T>::next(data);
+                return *this;
+            }
+            
+            MyRev_Iter_map operator++(int)
+            {
+                MyRev_Iter_map temp(data);
+                data = RedBlackTree<T>::next(data);
+                return temp;
+            }
+
+            MyRev_Iter_map &operator--()
+            {
+                data = RedBlackTree<T>::prev(data);
+                return *this;
+            }
+            
+            MyRev_Iter_map operator--(int)
+            {
+                MyRev_Iter_map temp(data);
+                data = RedBlackTree<T>::prev(data);
+                return temp;
+            }
+
+            bool operator==(const pointer cmp) const
+            {
+                return data->value->first == cmp->value->first;
+            }
+
+            bool operator!=(const pointer cmp) const
+            {
+                return data->value->first != cmp->value->first;
+            }
+            
+    };
 }
